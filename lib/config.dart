6ca36ea6e58ee0c891b28afbc80549af251e1113
Ali Sayar <alisayar07@gmail.com>
2022-07-29 01:58:43 +0300

    config dosyası

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+class Config{
+  static const String apiURL = "10.0.0.7:5432";
+  static const String loginAPI = "/api/login";
+  static const String registerAPI = "/api/register";
+  static const String userAPI = "/user/user-profile";
+}
