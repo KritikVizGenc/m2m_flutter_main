404b07ee3d91cfcb38e4bc3c9fd49c6e7ebdb3c6
Ali Sayar <alisayar07@gmail.com>
2022-07-29 01:58:32 +0300

    api service yazıldı

---
+++ lib/service/api_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:m2m_flutter_main/config.dart';
+import 'package:m2m_flutter_main/model/login_request_model.dart';
+import 'package:m2m_flutter_main/model/login_response_model.dart';
+import 'package:m2m_flutter_main/service/shared_service.dart';
+
+class APIService {
+  static var client = http.Client();
+
+      static Future<bool> login(LoginRequestModel model) async {
+        Map<String, String> requestHeaders = {
+          'Content-Type': 'application/json',
+        };
+
+        var url = Uri.http(Config.apiURL, Config.loginAPI);
+
+        var response = await client.post(
+          url,
+          headers: requestHeaders,
+          body: jsonEncode(model.toJson()),
+        );
+
+        if(response.statusCode == 200){
+          await SharedService.setLoginDetails(loginResponseJson(response.body));
+          return true;
+        } else {
+          return false;
+        }
+
+
+      }
+}
