1337529c0b3924a868c6f737ee3143af1f2a3a1d
Ali Sayar <alisayar07@gmail.com>
2022-07-29 13:21:18 +0300

    register api service

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:m2m_flutter_main/config.dart';
 import 'package:m2m_flutter_main/model/login_request_model.dart';
 import 'package:m2m_flutter_main/model/login_response_model.dart';
+import 'package:m2m_flutter_main/model/register_request_model.dart';
 import 'package:m2m_flutter_main/service/shared_service.dart';
 
 class APIService {
@@ -30,5 +31,25 @@ class APIService {
         }
 
 
+      }
+
+
+      static Future<bool> register(RegisterRequestModel model) async {
+        Map<String, String> requestHeaders = {
+          'Content-Type': 'application/json',
+        };
+        var url = Uri.http(Config.apiURL, Config.registerAPI);
+        var response = await client.post(
+          url,
+          headers: requestHeaders,
+          body: jsonEncode(model.toJson()),
+        );
+
+        if(response.statusCode == 200){
+          await SharedService.setLoginDetails(loginResponseJson(response.body));
+          return true;
+        } else {
+          return false;
+        }
       }
 }
