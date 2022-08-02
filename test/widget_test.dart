82b9667091f1e5de6c15afe201cef06b46c92e13
Ali Sayar <alisayar07@gmail.com>
2022-08-03 01:34:03 +0300

    Test Otomatik Düzenleme

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:m2m_flutter_main/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(LoginUIApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
