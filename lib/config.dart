6c1e9e549247d0a960e82b703cccdfd346635f4d
Ali Sayar <alisayar07@gmail.com>
2022-08-09 15:19:22 +0300

    endpoint

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,11 @@
 class Config{
-  static const String apiURL = "192.168.150.128:5000";
+  static const String apiURL = "192.168.43.65:5000";
   static const String loginAPI = "/api/login";
   static const String registerAPI = "/api/register";
-  static const String userAPI = "/user/user-profile";
+  static const String getUserAPI = "/api/getById/";
+  static const String getAllTags = "/api/getAllTag";
+  static const String getMyMentees = "/api/myMentees/";
+  static const String getMyMentors = "/api/myMentors/";
+  static const String connectAPI = "/api/reqMentors/";
+  static const String updateUserAPI = "/api/updateUser//";
 }
