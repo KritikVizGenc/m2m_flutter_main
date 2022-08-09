413ad49935eaae409b1651bd45c3217ab3d6e0a7
kaniksena <74830809+kaniksena@users.noreply.github.com>
2022-08-09 10:25:25 +0300

    bottombar

---
+++ lib/global.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:m2m_flutter_main/pages/main_page.dart';
+import 'package:m2m_flutter_main/pages/meeting_pages.dart';
+import 'package:m2m_flutter_main/pages/mentee_page.dart';
+import 'package:m2m_flutter_main/pages/mentor_page.dart';
+import 'package:m2m_flutter_main/pages/notification.dart';
+import 'package:m2m_flutter_main/pages/profile_page.dart';
+
+class Variable {
+  static int bottomBarIndex = 0;
+  static List<Widget> sayfalar = [
+    MainPage(),
+    ProfilePage(nereyeId: 3),
+    MeetPages(),
+    MenteePage(),
+    MentorPage(),
+  ];
+}
